c3fbfc394bb74a10fdc390f54b840483b9504f01
Divisek <113785426+Divisek@users.noreply.github.com>
2023-01-22 18:01:18 +0100

    Add files via upload

---
+++ fullscreen_shaDows_autosave/fullscreen_shaDows_autosave/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+string konfig = "Fullscreen shaDows autosave";
+konfig = konfig.ToLower();
+Console.WriteLine("Poběží hra ve fullscreenu?");
+Console.WriteLine(konfig.Contains("Fullscreen"));
+Console.WriteLine("Budou zapnuté stíny?");
+Console.WriteLine(konfig.Contains("shadows"));
+Console.WriteLine("Přeje si hráč vypnout zvuk?");
+Console.WriteLine(konfig.Contains("nosound"));
+Console.WriteLine("Přeje si hráč automaticky hru ukládat?");
+Console.WriteLine(konfig.Contains("autosave"));
+Console.ReadKey();
